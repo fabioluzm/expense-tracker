a89c0b07825a3eebf8e3bd26755468ccd908f32e
fabioluzm <fabioluzm@gmail.com>
2020-06-20 18:01:47 +0100

    add app controllers and update server routes

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const { getTransactions, addTransaction, deleteTransaction } = require('../controllers/transaction');
 
-router.get('/', (req, res) => res.send('Hello'));
+router
+  .route('/')
+  .get(getTransactions)
+  .post(addTransaction);
+
+router
+  .route('/:id')
+  .delete(deleteTransaction);
 
 module.exports = router;
