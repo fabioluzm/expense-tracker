30298d6654bb7d49ca5091f769b1270d94210dc9
fabioluzm <fabioluzm@gmail.com>
2020-07-03 16:07:15 +0100

    resolve bug on password authentication

--- config/auth.js
+++ config/auth.js
@@ -3,20 +3,8 @@ const bcrypt = require('bcryptjs');
 const generateHash = (password) => {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 }
-// module.exports = generateHash;
 
-// bcrypt.genSalt(10, function(err, salt) {
-//   bcrypt.hash("B4c0/\/", salt, function(err, hash) {
-//       // Store hash in your password DB.
-//   });
-// });
-
-// bcrypt.hash('bacon', 8, function(err, password) {
-// });
-
-
-
-const validatePassword = (password) => {
-  return bcrypt.compareSync(password);
+const validatePassword = (inputPassword, dbPassword) => {
+  return bcrypt.compareSync(inputPassword, dbPassword);
 }
 module.exports = {generateHash, validatePassword};
